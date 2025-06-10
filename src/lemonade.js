5867b3035558d1cc9d46500469d8efe1a4386144
JofaTech <80646647+JofaTech@users.noreply.github.com>
2025-06-09 21:47:10 -0400

    Added functionality to the lemonade order page of the website

--- src/lemonade.js
+++ src/lemonade.js
@@ -1,8 +1,70 @@
-const example = string => {
-    console.log(string)
+let state = {
+    lemonJuice: {
+        amount: 0,
+        max: 8,
+        measurement: 'ounces'
+    },
+    water: {
+        amount: 0,
+        max: 8,
+        measurement: 'ounces'
+    },
+    sugar: {
+        amount: 0,
+        max: 12,
+        measurement: 'tablespoons'
+    },
+    ice: {
+        amount: 0,
+        max: 15,
+        measurement: 'cubes'
+    }
 }
 
-let incrementButton = document.getElementById('incrementButton')
-console.log(incrementButton)
+const calcPercent = (amount, max, maxFill = 100) =>
+    100 - (amount / max) * maxFill
 
-incrementButton.addEventListener("click", () => example("Hello World"))
+const render = ({ lemonJuice, water, sugar, ice }) => {
+    $('#lemonJuice').css(
+        'transform',
+        `translate(0, ${calcPercent(lemonJuice.amount, lemonJuice.max)}%)`
+    )
+    $('#water').css(
+        'transform',
+        `translate(0, ${calcPercent(water.amount, water.max)}%)`
+    )
+    $('#sugar').css(
+        'transform',
+        `translate(0, ${calcPercent(sugar.amount, sugar.max, 40)}%)`
+    )
+    $('#lemonJuiceValue').html(lemonJuice.amount + ' ' + lemonJuice.measurement)
+    $('#waterValue').html(water.amount + ' ' + water.measurement)
+    $('#sugarValue').html(sugar.amount + ' ' + sugar.measurement)
+    $('#iceValue').html(ice.amount + ' ' + ice.measurement)
+}
+
+render(state)
+
+const updateIngredient = (name, { amount, max, measurement }, calcAmount) => {
+    state = {
+        ...state,
+        [name]: {
+            amount: calcAmount(amount, max),
+            max,
+            measurement
+        }
+    }
+}
+
+$('.increment').click((event) => {
+    updateIngredient(event.target.name, state[event.target.name], (amount, max) => amount < max ? amount + 1 : amount)
+    render(state)
+})
+
+$('.decrement').click((event) => {
+    updateIngredient(event.target.name, state[event.target.name], (amount) => amount > 0 ? amount - 1 : amount)
+    render(state)
+})
+
+
+
